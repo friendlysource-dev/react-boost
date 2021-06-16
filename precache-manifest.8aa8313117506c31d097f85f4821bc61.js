9534a90189e8862f911febc61b93abea75c5d0c6
Math W <mathewsto51@gmail.com>
2021-06-16 06:14:50 -0300

    Updates

---
+++ precache-manifest.8aa8313117506c31d097f85f4821bc61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f61db5eb3ae3e957356ce7e4b131bba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c14989c3bd418bfbc970",
+    "url": "./static/js/2.06ffe856.chunk.js"
+  },
+  {
+    "revision": "febe9a6103bd13688fccffa1c8d75c9f",
+    "url": "./static/js/2.06ffe856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c53b12eb83f989d626",
+    "url": "./static/js/main.4ba1a137.chunk.js"
+  },
+  {
+    "revision": "5ef783e0593ed6e0c854",
+    "url": "./static/js/runtime-main.7e54dbd2.js"
+  }
+]);
